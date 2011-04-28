c0ad4330210c51aaf0487f16672c25790b559009
Martin Korbel <korbel@gymjev.cz>
2011-04-28 21:09:22 +0200

    first commit

---
+++ examples/examples.js
@@ -0,0 +1,36 @@
+/* 
+ * List of examples in example dir
+ */
+
+var examplesList = [
+    'anklet',
+    'ball',
+    'diamond',
+    'flower2',
+    'floweroflife',
+    'flowers',
+    'grape',
+    'hex',
+    'hexboard',
+    'hilbert',
+    'iteratedsquare',
+    'iteratedsquare2',
+    'iteratedtriangle',
+    'kapradi',
+    'koch',
+    'linestar',
+    'octostar',
+    'pentaflake',
+    'polygons',
+    'polyspirala',
+    'pruzinky',
+    'sachovnice',
+    'sierpinskiL',
+    'spiralchain',
+    'spirals',
+    'stars',
+    'sun',
+    'tiling1'
+];
+
+
